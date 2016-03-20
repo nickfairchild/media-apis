ee43ce76eb3ec53229f518676f7d53b2424be3cd
Nick Fairchild <nick@wearethunderbolt.com>
2016-03-20 15:47:46 +0000

    fix class call

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -35,7 +35,7 @@ class MediaServiceProvider extends ServiceProvider
     {
         $this->registerTvdb();
 
-        $this->app->alias('tvdb', 'Media\TVDB');
+        $this->app->alias('tvdb', 'Media\TVDB\TVDB');
     }
 
     /**
@@ -59,6 +59,6 @@ class MediaServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-        return ['tvdb', 'Media\TVDB'];
+        return ['tvdb', 'Media\TVDB\TVDB'];
     }
 }
